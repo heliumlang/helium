41683d9873dbc6152270ddd99c4572dac283d2a4
Nykenik24 <Nykenik24@proton.me>
2026-05-10 23:16:17 +0200

    feat: use command args to pass filename

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Nykenik24/oxy/internal/runner"
 )
 
 func main() {
 	run := runner.New()
 
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: oxy <filename>")
+		os.Exit(1)
+	}
+
 	run.Benchmark(func() {
-		run.RunString(`module main
-variant StringOrInt {
-	string s
-	int i
-}`)
+		run.RunFile(os.Args[1])
 	})
 }
